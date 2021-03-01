8f20bd65b7ace6475b53f6affb05e585c4544bb0
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-03-01 07:04:42 -0500

    Update MediaShare.py

--- MediaShare.py
+++ MediaShare.py
@@ -3,6 +3,5 @@ auth = tweepy.OAuthHandler("CONSUMER KEY HERE", "CONSUMER KEY SECRET HERE")
 auth.set_access_token("ACCESS TOKEN HERE", "ACCESS TOKEN SECRET HERE")
 api = tweepy.API(auth)
 tweet = input("")
-image="C:/Users/facadmin1/Downloads/introdev.PNG"  #Enter image location
-api.update_with_media(image, tweet)
-print ("Done!")
+image="Enter your local image folder location"
+api.update_with_media(image, tweet
